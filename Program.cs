831d3adb0084d939372ad8293b1861e3f02bcb32
Korisnik <Korisnik@DESKTOP-Q60U0Q5>
2022-09-12 11:49:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string niz,niz1;
+            Console.WriteLine("Unesite niz znakova: ","\n");
+                niz=Console.ReadLine();
+            niz1=niz.Replace(" ", "_");
+            Console.WriteLine(niz1);
+            Console.ReadLine();
+        }
+    }
+}
